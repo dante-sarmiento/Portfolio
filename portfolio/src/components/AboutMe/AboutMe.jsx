bde002bcb2a0474661f42584cf1b2e4db6bb9921
dante-sarmiento <dante.sarmientof5@gmail.com>
2022-05-24 19:15:16 -0300

    make it better

--- portfolio/src/components/AboutMe/AboutMe.jsx
+++ portfolio/src/components/AboutMe/AboutMe.jsx
@@ -5,12 +5,12 @@ import './AboutMe.css'
 export const AboutMe = () => {
     return (
         <>
-        <div className='contAbout'>
+        <div className='container contAbout'>
                 <div className='container about'>
                     <div className='contFoto animate__animated animate__backInLeft'>
                         <img src={PhotoDante} alt="" className='ImgDante' />
                     </div>
-                    <div className='contText animate__animated animate__backInRight'>
+                    <div className='contText'>
                         <h3 className='h3text'>MY NAME IS</h3>
                         <h2 className='h2text'>DANTE SARMIENTO</h2>
                         <h3 className='h3leyend'>i'M A FULL STACK DEVELOPER</h3>
